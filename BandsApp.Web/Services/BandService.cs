dc7ba803aa4d2667995bb3ee2d5b387264f64d15
NiklasMattsson <niklasmattson1@gmail.com>
2025-05-13 11:07:04 +0200

    la till beskrivning

--- BandsApp.Web/Services/BandService.cs
+++ BandsApp.Web/Services/BandService.cs
@@ -5,9 +5,9 @@ namespace BandsApp.Web.Services;
 public class BandService
 {
     private List<Band> bands = [
-        new Band{ Id = 1, Name = "Spice Girls", Description = "", ImageUrl="spice_girls.jpg" },
-        new Band{ Id = 2, Name = "Westlife", Description = "", ImageUrl="westlife.jpg"},
-        new Band{ Id = 3, Name = "The Real Group", Description = "", ImageUrl = "The_Real_Group.jpg"},
+        new Band{ Id = 1, Name = "Spice Girls", Description = "Spice Girls är ett brittiskt popband som blev världskända på 90-talet med sitt budskap om \"girl power\".", ImageUrl="spice_girls.jpg" },
+        new Band{ Id = 2, Name = "Westlife", Description = "Westlife är ett irländskt pojkband känt för sina romantiska hits och stora internationella framgångar.", ImageUrl="westlife.jpg"},
+        new Band{ Id = 3, Name = "The Real Group", Description = "The Real Group är en svensk a cappella-grupp känd för sin jazziga stil och vokala perfektion.", ImageUrl = "The_Real_Group.jpg"},
         ];
 
     public Band? GetBandById(int id)
