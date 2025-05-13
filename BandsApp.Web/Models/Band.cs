b1fcba58b77a782930b7c48a26baeaf6aa7ba785
Astrid Grytberg <25738809+agrvsk@users.noreply.github.com>
2025-05-13 11:04:19 +0200

    bilder

--- BandsApp.Web/Models/Band.cs
+++ BandsApp.Web/Models/Band.cs
@@ -5,4 +5,6 @@ public class Band
     public int Id { get; set; }
     public string Name { get; set; } = null!;
     public string Description { get; set; } = null!;
+
+    public string ImageUrl { get; set; } = null!; 
 }
--- BandsApp.Web/Services/BandService.cs
+++ BandsApp.Web/Services/BandService.cs
@@ -5,9 +5,9 @@ namespace BandsApp.Web.Services;
 public class BandService
 {
     private List<Band> bands = [
-        new Band{ Id = 1, Name = "Spice Girls", Description = ""},
-        new Band{ Id = 2, Name = "Westlife", Description = ""},
-        new Band{ Id = 3, Name = "The Real Group", Description = ""},
+        new Band{ Id = 1, Name = "Spice Girls", Description = "", ImageUrl="spice_girls.jpg" },
+        new Band{ Id = 2, Name = "Westlife", Description = "", ImageUrl="westlife.jpg"},
+        new Band{ Id = 3, Name = "The Real Group", Description = "", ImageUrl = "The_Real_Group.jpg"},
         ];
 
     public Band? GetBandById(int id)
